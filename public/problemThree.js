cdd681c38a2f8f73aa037019b1eb9b8296b4cc0c
Trista Lu <t09b02307@ntu.edu.tw>
2024-05-06 06:20:00 -0400

    insert xss detection fot quesiton three done

--- public/problemThree.js
+++ public/problemThree.js
@@ -1,9 +1,28 @@
 const input = document.getElementById("result");
+const userInput = document.getElementById("xss-input");
+const button = document.getElementById("xssBtn");
+let lastCookie = document.cookie; // store cookie for comparison
 function handleCookieChange() {
   input.innerHTML = "you are admin";
 }
 
-let lastCookie = document.cookie; // store cookie for comparison
+function checkAnswer() {}
+
+function trigger() {
+  let text = userInput.value;
+  userInput.value = "";
+  console.log("trigger function");
+  if (
+    text.includes("<script>") &&
+    text.includes("document.cookie") &&
+    text.includes("</script>")
+  ) {
+    alert(lastCookie);
+    console.log(text);
+  }
+}
+
+button.addEventListener("click", trigger);
 
 setInterval(() => {
   if (document.cookie !== lastCookie) {
